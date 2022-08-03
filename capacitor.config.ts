f5217f13e3f97d20e6d8bc4e175ae1cbb4f278c1
Your Name <nhatlongsinh@gmail.com>
2022-08-03 21:17:27 +1000

    update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.appcontify.starter',
   appName: 'mobile-test',
   webDir: 'build',
   bundledWebRuntime: false
